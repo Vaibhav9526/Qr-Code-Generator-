ffb0a9d5136b8c29891f2dba75a0c403f166b2c4
Vaibhav Sharma <81508584+Vaibhav9526@users.noreply.github.com>
2022-06-19 13:05:28 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import QrImage from "../assests/QrCode.png";
+import QrImage from "https://s3.amazonaws.com/cdnl.iconscout.com/lottie/free/preview/3664074.gif?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA4VMJAKC54WWWCYMY%2F20220619%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220619T073253Z&X-Amz-SignedHeaders=host&X-Amz-Expires=232027&X-Amz-Signature=54b9b5739b9311c6ccd5cd654899a40a1c0b3ebca57ab791917b0bde2366464b";
 
 const Home = () => {
   return (
