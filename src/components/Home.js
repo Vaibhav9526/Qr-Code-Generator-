9f68769c08409c01948d85eedc250ee2eddd6172
Vaibhav Sharma <81508584+Vaibhav9526@users.noreply.github.com>
2024-11-03 18:28:49 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     <>
       <div className="home_container">
         <div className="home_image_container">
-          <img id="home_image" src="https://s3.amazonaws.com/cdnl.iconscout.com/lottie/free/preview/3664074.gif?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA4VMJAKC54WWWCYMY%2F20220619%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220619T073253Z&X-Amz-SignedHeaders=host&X-Amz-Expires=232027&X-Amz-Signature=54b9b5739b9311c6ccd5cd654899a40a1c0b3ebca57ab791917b0bde2366464b" alt="Home" />
+          <img id="home_image" src="https://img.freepik.com/free-vector/tiny-people-using-qr-code-online-payment-isolated-flat-illustration_74855-11136.jpg" alt="Home" />
         </div>
         <div className="start">
           <Link to="/QrCode">
